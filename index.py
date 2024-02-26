b8e1a2e9207d8dff9b1f579eec2f01078eb7d245
Guilherme Lins <guilhermelins978@gmail.com>
2024-02-26 19:44:09 -0300

    criei !perfil

--- index.py
+++ index.py
@@ -20,7 +20,6 @@ async def on_ready():
 # Evendo de boas vindas.
 @client.event
 async def on_member_join(member):
-  # Pegando o canal de boas vindas
   canalbemvindo = client.get_channel(1211027616845271150)
   # Pegando o cargo
   role = member.guild.get_role(1211106560869408838)
@@ -34,7 +33,7 @@ async def on_message(message):
     # Criar uma embed
     embed = discord.Embed(
       title = "Lista de Comandos:",
-      description = "- !comandos\n- !teste\n- !teste2\n- !teste3\n- !teste4\n- !teste5\n- !teste6",
+      description = "- !comandos\n- !perfil",
       color = 0xffff00
     )
     embed.set_author(
@@ -47,7 +46,35 @@ async def on_message(message):
     await message.channel.send(embed=embed)
     time.sleep(10)
 
- 
+  elif message.content == "!perfil":
+    print(message)
+    embed = discord.Embed(
+      title = message.author.global_name,
+      description = "Esse é o seu perfil",
+      color = 0x993399
+    )
+    roles = ""
+    for role in message.author.roles:
+      if role.name != "@everyone":
+        roles = f"- {role.name}\n" + roles
+        print(roles)
+    embed.add_field(
+        name="Cargos:",
+        value=roles,
+        inline= False
+      )
+    embed.set_author(
+      name=f"Perfil de {client.user.name}", 
+      icon_url=client.user.avatar.url)
+    embed.set_image(
+      url=message.author.avatar.url
+    )
+    embed.set_footer(
+      text="created by Kinz015",
+      icon_url=client.user.avatar.url
+    )
+    await message.channel.send(embed=embed)
+
   elif message.content == "!teste": 
     await message.channel.send(f"Testado!")
 
