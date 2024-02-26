bcb5cd4fa2562c97eb50e938b4416165ca524ec0
Guilherme Lins <guilhermelins978@gmail.com>
2024-02-26 16:17:39 -0300

    adicionei o embed

--- index.py
+++ index.py
@@ -22,7 +22,7 @@ async def on_member_join(member):
   # Pegando o canal de boas vindas
   canalbemvindo = client.get_channel(1211027616845271150)
   # Pegando o cargo
-  role = client.get_guild(1156724799423389736).get_role(1211106560869408838)
+  role = member.guild.get_role(1211106560869408838)
   mensagem = await canalbemvindo.send(f"{member.mention} bem vindo mamaco.")
   await member.add_roles(role)
 
@@ -30,8 +30,20 @@ async def on_member_join(member):
 @client.event
 async def on_message(message):
   if message.content == "!comandos":
-    await message.channel.send(f"!comandos \n!teste")
+    # Criar uma embed
+    embed = discord.Embed(
+      title = "Lista de Comandos:",
+      description = "- !comandos\n- !teste\n- !teste2\n- !teste3\n- !teste4\n- !teste5\n- !teste6",
+      color = 0xffff00
+    )
+    embed.set_footer(
+      text="create by Kinz015",
+      icon_url="https://pics.craiyon.com/2023-11-13/uMOzhnlCSe2Ri4P0kpR6-A.webp"
+    )
+    await message.channel.send(embed=embed)
+    
   elif message.content == "!teste": 
     await message.channel.send(f"Testado!")
+
 
 client.run(TOKEN)
